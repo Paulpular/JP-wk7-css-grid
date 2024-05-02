bb6f2cc9d380576c0f7b3c35187ee7236d7e5e66
Paulpular <156431389+Paulpular@users.noreply.github.com>
2024-05-02 22:46:31 +0100

    commit

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
-const hamburger = document.querySelector('.menu')
-const nav = document.querySelector('.second-header')
+const hamburger1 = document.querySelector('.menu-list')
+const hamburger2 = document.querySelector('.menu-X')
+const mobile_menu = document.querySelector('.second-header')
 const div = document.querySelector('.grid-container2')
 
 
-hamburger.addEventListener('click', ()=> {
-    nav.classList.toggle('visible')
-    div.classList.toggle('close')
+hamburger1.addEventListener('click', ()=> {
+    hamburger1.classList.add('invisible')
+    hamburger2.classList.add('visible')
+    mobile_menu.classList.toggle('switch')
+    div.classList.add('close')
+})
+
+hamburger2.addEventListener('click', ()=> {
+    hamburger1.classList.remove('invisible')
+    hamburger2.classList.remove('visible')
+    mobile_menu.classList.toggle('switch')
+    div.classList.remove('close')
 })
