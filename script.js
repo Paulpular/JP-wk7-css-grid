c07625c9dcc43a057d37d0d5fc5389c4f26aed73
Paulpular <156431389+Paulpular@users.noreply.github.com>
2024-05-02 16:47:21 +0100

    commit

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-const hamburger = document.querySelector('.list-image-frame')
+const hamburger = document.querySelector('.menu')
 const nav = document.querySelector('.second-header')
+const div = document.querySelector('.grid-container2')
 
 
 hamburger.addEventListener('click', ()=> {
     nav.classList.toggle('visible')
+    div.classList.toggle('close')
 })
